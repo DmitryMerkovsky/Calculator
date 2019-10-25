84b79092f32644c1f94e23a4e2dbde548a667fcf
Mijtin <mijtin@localhost.localdomain>
2019-10-25 13:23:55 +0700

    Structure update

--- functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef functions_h
-#define functions_h
-
-double subtraction(double x, double y);
-double addition(double x, double y);
-double multiplication(double x, double y);
-double division(double x, double y);
-
-#endif
